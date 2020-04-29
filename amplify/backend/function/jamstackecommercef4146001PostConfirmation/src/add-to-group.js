e2d8efc5777dc26b2e0f25f800e4f1a7031d2d5f
Glenn Lanzer <lanzertrey@gmail.com>
2020-04-28 23:41:30 -0500

    edit(service): function will add a user to the Admin group if their email is included in the adminEmails array
    - Edited add-to-group.js:
      - This is the code for the post-confirmation Lambda trigger

--- amplify/backend/function/jamstackecommercef4146001PostConfirmation/src/add-to-group.js
+++ amplify/backend/function/jamstackecommercef4146001PostConfirmation/src/add-to-group.js
@@ -1,28 +1,51 @@
-/* eslint-disable-line */ const aws = require('aws-sdk');
+/* eslint-disable-line */ const aws = require('aws-sdk')
 
+/**
+ * This function will add a user to the Admin group if
+ * their email is included in the `adminEmails` array
+ * @param event
+ * @param context
+ * @param callback
+ */
 exports.handler = async (event, context, callback) => {
-  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
-  const groupParams = {
-    GroupName: process.env.GROUP,
-    UserPoolId: event.userPoolId,
-  };
+  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider(
+    { apiVersion: '2016-04-18' }
+  )
 
-  const addUserParams = {
-    GroupName: process.env.GROUP,
-    UserPoolId: event.userPoolId,
-    Username: event.userName,
-  };
+  // Array of Admin emails you would like to use
+  let adminEmails = ['lanzertrey@gmail.com'],
+    isAdmin = false
 
-  try {
-    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
-  } catch (e) {
-    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
+  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+    isAdmin = true
   }
 
-  try {
-    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
-    callback(null, event);
-  } catch (e) {
-    callback(e);
+  if (isAdmin) {
+    const groupParams = {
+      GroupName: process.env.GROUP,
+      UserPoolId: event.userPoolId,
+    }
+
+    const addUserParams = {
+      ...groupParams,
+      Username: event.userName,
+    }
+
+    try {
+      await cognitoidentityserviceprovider.getGroup(groupParams).promise()
+    } catch (e) {
+      await cognitoidentityserviceprovider.createGroup(groupParams).promise()
+    }
+
+    try {
+      await cognitoidentityserviceprovider
+        .adminAddUserToGroup(addUserParams)
+        .promise()
+      callback(null, event)
+    } catch (e) {
+      callback(e)
+    }
+  } else {
+    callback(null, event)
   }
-};
+}

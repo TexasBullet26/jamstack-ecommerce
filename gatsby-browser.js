8e339cd92fff7e607c3eaf9fc64bfbaff11e14b3
Glenn Lanzer <lanzertrey@gmail.com>
2020-05-03 08:09:57 -0500

    Config: gatsby project to recognize amplify

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+// Configures our gatsby (client) project to recognize our amplify (backend) project
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+
 import './src/styles/site.css'
 import './src/layouts/layout.css'
+
+Amplify.configure(config)

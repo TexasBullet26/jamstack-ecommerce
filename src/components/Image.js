b39860b736f296cfe439d6eeed11d19e83636559
Glenn Lanzer <lanzertrey@gmail.com>
2020-05-04 01:02:56 -0500

    modify: Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react'
+import { Storage } from 'aws-amplify'
 
 async function fetchImage(src, updateSrc) {
-  // const image = await S3.getimage(src)
-  updateSrc(src)
+  // check to see if the image is a locally downloaded image (in downloads)
+  if (!src.includes('downloads')) {
+    // if image is not locally downloaded, we know it is a remote
+    // image from S3 and we will fetch the signed URL for the image
+    const image = await Storage.get(src)
+    // const image = await S3.getimage(src)
+    updateSrc(image)
+  } else {
+    updateSrc(src)
+  }
 }
 
 const Image = ({ src, ...props }) => {
